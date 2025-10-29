6f37d78229f6658ba3580e1f0e1d9d8acd875efa
joshardals <joshuabamidele219@gmail.com>
2025-10-29 01:31:04 +0100

    first of many commits

---
+++ components/CheckoutForm.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function CheckoutForm() {
+  return <div>CheckoutForm</div>;
+}
---
+++ components/PostcardCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export  function PostcardCard() {
+  return (
+    <div>PostcardCard</div>
+  )
+}
